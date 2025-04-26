3bbfd254d2cd28c3f863ca04059d15f3297b7cec
yahyokhoja <yahuokhoja@gmail.com>
2025-04-26 11:42:45 +0500

    popravka

--- app.py
+++ app.py
@@ -2,18 +2,25 @@ import requests
 from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
+
 @app.route('/price')
 def get_price():
     try:
         url = "https://api.bybit.com/v5/market/tickers?category=linear&symbol=BTCUSDT"
         res = requests.get(url)
         if res.status_code != 200:
-            return jsonify({'error': 'Ошибка при запросе к API Bybit: ' + str(res.status_code)})
+            return jsonify({'error': f'Ошибка при запросе к API Bybit: {res.status_code}'})
         
         data = res.json()
-        price = data['result']['list'][0]['lastPrice']
+        tickers = data.get('result', {}).get('list', [])
+        if not tickers:
+            return jsonify({'error': 'Пустой список тикеров'})
+        
+        price = tickers[0].get('lastPrice', 'неизвестно')
         return jsonify({'price': price})
     except Exception as e:
+        import traceback
+        traceback.print_exc()  # добавим вывод ошибки в логи
         return jsonify({'error': f"Ошибка: {str(e)}"})
 
 
